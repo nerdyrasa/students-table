17bb8deb93bda30c4fc874528a5e690e28d41218
nerdyrasa <nerdyrasa@users.noreply.github.com>
2016-11-10 15:48:12 -0600

    Added pagination code

--- js/app.js
+++ js/app.js
@@ -1,21 +1,12 @@
-
-// const keyword is not supported in IE so just use var instead
 var NUMBER_OF_STUDENTS_PER_PAGE = 10;
-
 var $ul_li = $("ul li");
-
 var numberOfStudents = $ul_li.length;
-
-console.log("number of students", numberOfStudents);
-
-goToPage(1);
-
 var numberOfPages = Math.ceil( numberOfStudents / NUMBER_OF_STUDENTS_PER_PAGE );
 
-console.log("number of pages", numberOfPages);
+createPaginationLinks();
+goToSpecificPage(1);
 
-function goToPage(pageNumber){
-
+function goToSpecificPage(pageNumber) {
   var lowerBound = ( pageNumber - 1 ) * 10;
   var upperBound = pageNumber * NUMBER_OF_STUDENTS_PER_PAGE;
 
@@ -31,4 +22,50 @@ function goToPage(pageNumber){
       $(this).css("display", "none");
     }
   });
+}
+
+function goToPage(){
+  pageNumber = $(this).text();
+  goToSpecificPage(pageNumber);
+}
+
+
+//<div class="pagination">
+//  <ul>
+//  <li>
+//  <a class="active" href="#">1</a>
+//  </li>
+//  <li>
+//  <a href="#">2</a>
+//  </li>
+//  <li>
+//  <a href="#">3</a>
+//  </li>
+//  <li>
+//  <a href="#">4</a>
+//  </li>
+//  <li>
+//  <a href="#">5</a>
+//  </li>
+//  </ul>
+//  </div>
+//    <!-- end pagination -->
+//
+//  </div>
+
+// Got some help from https://www.youtube.com/watch?v=Wxnd21_f_pc
+//
+
+function createPaginationLinks() {
+
+  var paginationDiv = $("<div class='pagination'><ul><li></li></ul></div>");
+
+  $(".page").append(paginationDiv);
+
+  for (var i=1; i <= numberOfPages; i++) {
+    console.log(i);
+    var pageLink = $("<a href='#'>" + i + "</a>").on("click", goToPage);
+    $(".pagination ul li").append(pageLink);
+  }
+
 }
